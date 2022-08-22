0f888412d8c79d09b5a3145d206b92deed81078d
root <son.vu2@sotatek.com>
2022-08-22 15:08:15 +0700

    fix: fix call notices and reports

--- src/helper/handleResponse.ts
+++ src/helper/handleResponse.ts
@@ -4,29 +4,23 @@ import { getReport } from "./report";
 // Call notices (fulfilled) and reports (rejected) to get response (If it fail, continue calling. Maximum number of call: 20)
 export const handleResponse = async (epoch: number | undefined, input: number | undefined, callback: any) => {
     var times = 0;
-
     const myInterval = setInterval(async () => {
         try {
-            let result
-            const notices: any = await getNotice({
+            const notice = await getNotice({
                 epoch,
                 input
             })
-            // const convertNotice = JSON.parse(notices)
-            // let convertReport
-            // if (convertNotice?.length !== 0) {
-            //     const reports: any = await getReport({
-            //         epoch,
-            //         input
-            //     })
-            //     convertReport = JSON.parse(reports)
-            // }
-
-            result = notices
-            const arr = JSON.parse(result)
-            console.log(`Number to calls notices: ${times}, call result:`, arr.length > 0 ? true : false)
-            if (arr.length > 0) {
-                const payload = JSON.parse(arr[0]?.payload)
+            const report = await getReport({
+                epoch,
+                input
+            })
+            let [msgNotice, msgReport] = await Promise.all([notice, report]);
+            const prsNotice = JSON.parse(msgNotice)
+            const prsReport = JSON.parse(msgReport)
+            const result = prsNotice.length > 0 ? prsNotice : prsReport
+            console.log(`Number to calls notices: ${times}, call result:`, result.length > 0 ? true : false)
+            if (result.length > 0) {
+                const payload = JSON.parse(result[0]?.payload)
                 if (payload) {
                     clearInterval(myInterval);
                     console.log('Call successful!')
